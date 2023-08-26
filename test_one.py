5590d4aa1de4f938de9a19912d37ed9b7d9d77b5
unknown <amareshkavadimatti@gmail.com>
2023-08-26 20:07:13 +0530

    added

--- test_one.py
+++ test_one.py
@@ -1,13 +1,5 @@
-from selenium import webdriver
-from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
 
-service = Service(GeckoDriverManager().install())
-# options = Options().add_experimental_option("detach", True)
 
 
 def test_one():
-    driver = webdriver.Firefox(service=service)
-    driver.get("https://www.google.com/")
-    assert driver
+    assert 1 == 1
